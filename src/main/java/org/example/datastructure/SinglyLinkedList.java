b129894ca3bfc1d1a136a1bca8d1d72b805d4805
Matheus Andreoli <80846922@redecorp.br>
2023-10-31 07:58:53 -0300

    loops logics

--- src/main/java/org/example/datastructure/SinglyLinkedList.java
+++ src/main/java/org/example/datastructure/SinglyLinkedList.java
@@ -1,12 +1,14 @@
 package org.example.datastructure;
 
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Objects;
 
 public class SinglyLinkedList {
     private ListNode head;
 
     public static class ListNode {
-        private int data;
+        private final int data;
         private ListNode nextNode;
 
         public ListNode(int data) {
@@ -107,7 +109,131 @@ public class SinglyLinkedList {
         current.nextNode = temp.nextNode;
     }
 
-    public static void main(String[] args) {
+    public boolean find(int value) {
+        if (head == null) {
+            return false;
+        }
+        ListNode current = head;
+        while (current != null) {
+            if (current.data == value) return true;
+            current = current.nextNode;
+        }
+        return false;
+    }
+
+    public void reverse() {
+        if (isHeadNull()) return;
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next;
+        while (current != null) {
+            next = current.nextNode;
+            current.nextNode = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+    }
+
+    public ListNode findMiddleNode() {
+        if (isHeadNull()) return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (isCurrentNodeAndNextNotNull(fast)) {
+            slow = slow.nextNode;
+            fast = fast.nextNode.nextNode;
+        }
+        return slow;
+    }
+
+    public ListNode getNthNodeFromEnd(int n) throws IllegalAccessException {
+        if (isHeadNull()) return null;
+        if (n <= 0) throw new IllegalAccessException("Invalid value. Must be a positive number");
+        ListNode main = head;
+        ListNode reference = head;
+        int count = 0;
+        while (count < n) {
+            if (reference == null)
+                throw new IllegalAccessException("Given value is greater then SinglyLinkedList length");
+            reference = reference.nextNode;
+            count++;
+        }
+        while (reference != null) {
+            reference = reference.nextNode;
+            main = main.nextNode;
+        }
+        return main;
+    }
+
+    public void removeDuplicates() {
+        ListNode current = head;
+        while (isCurrentNodeAndNextNotNull(current)) {
+            if (current.data == current.nextNode.data) {
+                current.nextNode = current.nextNode.nextNode;
+            } else current = current.nextNode;
+        }
+    }
+
+    public void removeKey(int key) {
+        if (isHeadNull()) return;
+        ListNode current = head;
+        ListNode temp = head;
+        if (current.data == key) {
+            head = current.nextNode;
+            return;
+        }
+        while (current != null && current.data != key) {
+            temp = current;
+            current = current.nextNode;
+        }
+        if (current == null) return;
+        temp.nextNode = current.nextNode;
+    }
+
+    public boolean containsLoop() {
+        if (isHeadNull()) return false;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (isCurrentNodeAndNextNotNull(fast)) {
+            fast = fast.nextNode.nextNode;
+            slow = slow.nextNode;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+
+    public ListNode startNodeInALoop() {
+        if (isHeadNull()) return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (isCurrentNodeAndNextNotNull(fast)) {
+            fast = fast.nextNode.nextNode;
+            slow = slow.nextNode;
+            System.out.println("slow = " + slow.data);
+            System.out.println("fast = " + fast.data);
+            if (fast == slow) return getStartingNode(slow);
+        }
+        return null;
+    }
+
+    private ListNode getStartingNode(ListNode slow) {
+        ListNode temp = head;
+        while (temp != slow) {
+            temp = temp.nextNode;
+            slow = slow.nextNode;
+        }
+        return temp;
+    }
+
+    private boolean isCurrentNodeAndNextNotNull(ListNode node) {
+        return node != null && node.nextNode != null;
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
         System.out.println("SinglyLinkedList");
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
         singlyLinkedList.head = new ListNode(10);
@@ -136,7 +262,44 @@ public class SinglyLinkedList {
         singlyLinkedList.add(23);
         singlyLinkedList.display();
         singlyLinkedList.deleteAt(3);
+        singlyLinkedList.add(43);
+        singlyLinkedList.addValueAt(87, 5);
         singlyLinkedList.display();
+        int value = 99;
+        System.out.printf("Found %s in singlyLinkedList? %s%n", value, singlyLinkedList.find(value));
+        singlyLinkedList.reverse();
+        singlyLinkedList.unshift(43);
+        singlyLinkedList.addValueAt(23, 3);
+        singlyLinkedList.add(10);
+        singlyLinkedList.display();
+        singlyLinkedList.removeDuplicates();
+        singlyLinkedList.display();
+        singlyLinkedList.removeKey(43);
+        singlyLinkedList.removeKey(87);
+        singlyLinkedList.display();
+        System.out.println("singlyLinkedList.getNthNodeFromEnd(n) = " + singlyLinkedList.getNthNodeFromEnd(6).data);
+        System.out.println("singlyLinkedList.findMiddleNode() = " + singlyLinkedList.findMiddleNode().data);
         System.out.println("singlyLinkedList.length() = " + singlyLinkedList.length());
+
+        System.out.println("========================= Loop SinglyLinkedList =========================");
+        SinglyLinkedList loop = new SinglyLinkedList();
+        loop.head = new ListNode(1);
+        ListNode value2 = new ListNode(2);
+        ListNode value3 = new ListNode(3);
+        ListNode value4 = new ListNode(4);
+        ListNode value5 = new ListNode(5);
+        loop.head.nextNode = value2;
+        value2.nextNode = value3;
+        value3.nextNode = value4;
+        value4.nextNode = value5;
+        value5.nextNode = value2;
+        /*
+        Visual of loop:
+        1 --> 2 --> 3
+              |     |
+              5 <-- 4
+         */
+        System.out.printf("Found a loop in singlyLinkedList? %s%n", loop.containsLoop());
+        System.out.println("loop.startNodeInALoop() = " + loop.startNodeInALoop().data);
     }
 }
